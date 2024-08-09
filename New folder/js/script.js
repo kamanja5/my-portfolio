0446e327ca2d29599a317972835e55f60d8175cf
Kamanja5 <138703488+kamanja5@users.noreply.github.com>
2024-08-09 23:42:18 +0300

    changed typing js

--- New folder/js/script.js
+++ New folder/js/script.js
@@ -1,9 +1,9 @@
 
 var typed = new Typed('#skills',{
     strings: ["Web Developer", "Freelancer", "Web Designer"],
-    typeSpeed: 30,
-    backSpeed: 30,
-    backDelay: 1500,
+    typeSpeed: 20,
+    backSpeed: 20,
+    backDelay: 1700,
     loop: true,
     cursorChar: '|',  // This sets the character used as the cursor
 });
