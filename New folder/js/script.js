deb25e51cadf7ca2b2919461b779c118150e312e
Kamanja5 <138703488+kamanja5@users.noreply.github.com>
2024-08-09 23:33:29 +0300

    first commit

---
+++ New folder/js/script.js
@@ -0,0 +1,9 @@
+
+var typed = new Typed('#skills',{
+    strings: ["Web Developer", "Freelancer", "Web Designer"],
+    typeSpeed: 30,
+    backSpeed: 30,
+    backDelay: 1500,
+    loop: true,
+    cursorChar: '|',  // This sets the character used as the cursor
+});
