7e4ed94a4a53fdd72787b78b835bbd6d710c93c0
Kamanja5 <138703488+kamanja5@users.noreply.github.com>
2024-08-11 23:20:51 +0300

    changed typespeed

--- New folder/js/script.js
+++ New folder/js/script.js
@@ -3,7 +3,7 @@ var typed = new Typed('#skills',{
     strings: ["Web Developer", "Freelancer", "Web Designer"],
     typeSpeed: 20,
     backSpeed: 20,
-    backDelay: 1700,
+    backDelay: 1000,
     loop: true,
     cursorChar: '|',  // This sets the character used as the cursor
 });
