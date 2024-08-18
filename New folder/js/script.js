bc69504d51b6dfcfddd621bc15ef3c34bdac6974
Kamanja5 <138703488+kamanja5@users.noreply.github.com>
2024-08-19 02:01:25 +0300

    Added contact details

--- New folder/js/script.js
+++ New folder/js/script.js
@@ -1,8 +1,8 @@
 
 var typed = new Typed('#skills',{
     strings: ["Web Developer", "Freelancer", "Web Designer"],
-    typeSpeed: 20,
-    backSpeed: 20,
+    typeSpeed: 25,
+    backSpeed: 25,
     backDelay: 1000,
     loop: true,
     cursorChar: '|',  // This sets the character used as the cursor
