53aa79acfb2499201dce83af93c81ab7b6cdd9bd
Kamanja5 <138703488+kamanja5@users.noreply.github.com>
2024-08-22 02:08:22 +0300

    made buttons responsive

--- New folder/js/script.js
+++ New folder/js/script.js
@@ -1,9 +1,9 @@
 
 var typed = new Typed('#skills',{
     strings: ["Web Developer", "Freelancer", "Web Designer"],
-    typeSpeed: 25,
-    backSpeed: 25,
-    backDelay: 1000,
+    typeSpeed: 20,
+    backSpeed: 20,
+    backDelay: 1100,
     loop: true,
     cursorChar: '|',  // This sets the character used as the cursor
 });
