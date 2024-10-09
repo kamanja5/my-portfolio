2ad7f593c66c53116dd6c2ad80c1d8f5db262a48
Kamanja5 <138703488+kamanja5@users.noreply.github.com>
2024-10-09 15:46:53 +0300

    modified:   New folder/css/styles.css 	modified:   New folder/js/typed.js

--- New folder/js/typed.js
+++ New folder/js/typed.js
@@ -1,2 +1,17 @@
 var work = document.querySelector('.work');
 var mixer = mixitup(work)
+
+var loader = document.getElementById('preloader');
+window.addEventListener("load", function(){
+    loader.style.display = "none";
+    $('#preloader').delay(500).fadeOut(500);
+})
+
+$(window).on('scroll',function(){
+    var scroll = $(window).scrollTop();
+    if(scroll < 150){
+        $('.header').removeClass('sticky');
+    }else{
+        $('.header').addClass('sticky');
+    }
+});
